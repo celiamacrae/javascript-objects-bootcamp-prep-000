035ee2ac565387f21f36745a8b3e04e20f55ce3c
amazing-server-2710 <celiamacrae@gmail.com>
2019-07-09 15:16:46 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { BlackEyedPeas : "Boom Boom Pow"
+var playlist = { BlackEyedPeas : "Boom Boom Pow"}
 
 function updatePlaylist(playlist, artist, song){
   return Object.assign({}, playlist, {[artist]: song})
